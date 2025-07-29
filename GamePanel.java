6d83a56ab11c78b35d5693b8840c0a67a33f11bd
Anass1234561234 <anassboushaki2@gmail.com>
2025-07-29 21:27:02 +0000

    Update GamePanel.java
    affiche health of the pokemon et message tour

--- GamePanel.java
+++ GamePanel.java
@@ -5,13 +5,28 @@ import java.awt.Font;
 
 public class GamePanel {
     private Plateau plateau;
+    private Pokemon[][] grid;
     private Souris souris;
+    private Pokemon selected = null;
+    private Texte messageTour;
 
     public GamePanel(Plateau plateau) {
         this.plateau = plateau;
+        this.grid = plateau.getGrid();
+
         this.souris = new Souris(Plateau.TAILLE_CASE * 9);
         plateau.getFenetre().addMouseListener(souris);
         plateau.getFenetre().addMouseMotionListener(souris);
+
+        // ✅ Message de tour de joueur
+        messageTour = new Texte(
+            Couleur.ROUGE,
+            "Au joueur rouge de jouer",
+            new Font("Arial", Font.BOLD, 22),
+            new Point(Plateau.TAILLE_CASE * 2, Plateau.TAILLE_CASE * 9 - 25)
+        );
+        plateau.getFenetre().ajouter(messageTour);
+
     }
 }
 
